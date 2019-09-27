1a2ff90f20976e20cf008d4ab46b7b1756f2fbae
andryosa <andryos44@gmail.com>
2019-09-27 10:29:53 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+
+let city1 = {
+    name : 'Paris',
+    population : '120000',
+    continent : 'Europe',
+  }
+  
+  let city2 = {
+    name : 'Rome',
+    population : '120000',
+    continent : 'Europe',
+  }
+  
+  let city3 = {
+    name : 'Barcelona',
+    population : '120000',
+    continent : 'Europe',
+  }
+  
+  let city4 = {
+    name : 'Venetto',
+    population : '70000',
+    continent : 'Europe',
+  }
+  
+  let arrayofCity = [city1, city2, city3, city4];
+  let arrayofPreferences = ['1800000', 'Europe']
+  
+  function tripAdvisor(city){
+    if(city.population <= 80000){
+    return 'positiv'}
+    else{return 'negative'}
+    return city
+  }
+  
+  //console.log(tripAdvisor(city4))
+  
+  function advice(arr1, arr2){
+    for (i=0 ; i<arr1.length && i<arr2.length ; i++){
+      if (arr1.)
+  
+    }
+  }
