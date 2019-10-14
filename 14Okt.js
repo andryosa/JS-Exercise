ee5da06f4e020575623d11181c4d49c3bb81a101
andryosa <andryos44@gmail.com>
2019-10-14 11:19:36 +0200

    14OK5t2

--- 14Okt.js
+++ 14Okt.js
@@ -8,12 +8,26 @@
 
 let cities2 = ['Hamburg', 'Istanbul', 'Frankfurt']
 let citytoSearch = 'Istanbul'
+let check = ['Istanbul', 'Hamburg']
 
-function cityIvevisited(x,arr1){
-    for(i=0; i<arr1.length; i++)
-    if (arr1[i] == x){
-        return `I have visited ${x}`
-    }return `I have not visited ${x}`
+// function cityIvevisited(x,arr1){
+//     for(i=0; i<arr1.length; i++){
+//     if (arr1[i] == x){
+//         return `I have visited ${x}`
+//     }return `I have not visited ${x}`
+// }}
+
+// document.write(cityIvevisited( citytoSearch, cities2))
+
+function cityMatch(arr1,arr2){
+    let arr3 = [];
+    for(i=0; i<arr1.length; i++){
+        for(j=0; j<arr1.length; j++){
+            if (arr1[i] == arr2[j]){
+                arr3.push(arr2[j])
+                }
+        }
+    }return `I have visited ${arr3}`
 }
 
-document.write(cityIvevisited( citytoSearch, cities2))
+document.write(cityMatch(cities2, check))
