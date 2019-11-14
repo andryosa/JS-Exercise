fd07cdae551f399ce7324f81f763ec681382f94a
andryosa <andryos44@gmail.com>
2019-11-14 11:21:40 +0100

    s

---
+++ DOM-2_Selectors/app.js
@@ -0,0 +1,11 @@
+// document.getElementById()
+// console.log(document.getElementById('task-title'))
+
+// Get things from the element
+
+// console.log(document.getElementById('task-title').id)
+
+// Change the style
+document.getElementById('task-title').style.background = '#333'
+
+document.getElementById('task-title').style.color = '#fff'
