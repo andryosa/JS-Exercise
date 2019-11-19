04bc9a0c40200b7b38bf19355828e381fa13b5ca
andryosa <andryos44@gmail.com>
2019-11-19 09:49:13 +0100

    r

--- DOM-2_Selectors/app.js
+++ DOM-2_Selectors/app.js
@@ -5,7 +5,51 @@
 
 // console.log(document.getElementById('task-title').id)
 
-// Change the style
-document.getElementById('task-title').style.background = '#333'
 
-document.getElementById('task-title').style.color = '#fff'
+
+// ----------------------------------------------------
+// #CHANGING THE STYLE#
+
+// document.getElementById('task-title').style.background = '#333'
+
+// document.getElementById('task-title').style.color = '#fff'
+
+// document.getElementById('task-title').style.padding = '6px'
+
+// #or#
+
+const tasksTitle = document.getElementById('task-title');
+
+tasksTitle.style.background = 'greenyellow'
+
+tasksTitle.style.color = '#fff'
+
+tasksTitle.style.padding = '6px'
+
+
+// Change the Content
+tasksTitle.textContent = 'Task List'
+
+tasksTitle.innerText = 'Task List (innerText)'
+
+tasksTitle.innerHTML = '<span style="color:white">Task List</span>';
+
+document.querySelector('li').style.color = 'orange'
+
+document.querySelector('li:last-child').style.color = 'green'
+
+document.querySelector('li:nth-child(3)').style.color = 'purple'
+
+document.querySelector('li:nth-child(4)').textContent = 'List Number 4'
+
+document.querySelector('li:nth-child(odd)').style.background = 'purple'
+
+document.querySelector('li:nth-child(odd)').style.color = '#fff'
+
+document.querySelector('li:nth-child(even)').style.background = 'turquoise'
+
+
+// ----------------------------------------------------
+
+
+
